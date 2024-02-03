3a43e4c96bac93369e642af27df2ba8ef3b3b4ff
Valeriia04 <savcinvaleria@gmail.com>
2024-02-03 20:36:46 +0200

    cycles

--- Les05/src/Main.java
+++ Les05/src/Main.java
@@ -2,11 +2,51 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner cs = new Scanner(System.in);
+        Scanner cs = new Scanner(System.in);
 
-        //for (...;...;...)
-        //for(int i = 3;i < 5;i++){
-        //    System.out.println(i+") Hello");
-        // }
+
+        /*for(int i = 3;i < 5;i++){
+           System.out.println(i+") Hello");*/
+       /* System.out.println(" Enter number:");
+       int value = cs.nextInt();
+        for( int a=0; a<= value; a++  ){
+            System.out.println(" a:" +a);
+
+        }*/
+       /* int value = cs.nextInt();
+        for( int a=0; a<= value; a++  ){
+            System.out.print(" " +a);}
+        System.out.println();
+        for( int a=value; a>=0; a--  ){
+            System.out.print(" " +a);}
+        System.out.println();
+
+        for( int a=0; a<= value/2; a++  ){
+            System.out.print(" " +a);}
+        System.out.println();
+        for( int a= value/2; a<= value; a++  ){
+            System.out.print(" " +a);}
+        System.out.println();
+        for( int a= value; a>= value/2; a--  ){
+            System.out.print(" " +a);}*/
+
+       /* for( int a=0; a<=100 ; a+=2  ){
+            System.out.print(" " +a);}*/
+        System.out.println( " Enter first number :");
+        int a = cs.nextInt();
+        System.out.println( " Enter second number :");
+        int b = cs.nextInt();
+        if(a<b) {
+            for (int i = a; i <= b; i++) {
+                System.out.println(" " + i);
+            }
+        }
+        else {
+            for (int i = a; i >= b; i--) {
+                System.out.println(" " + i);
+
+            }
+        }
+
     }
 }
