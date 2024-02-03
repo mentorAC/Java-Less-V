29f1fb802f3d8408e7af57bfe3721365fa25c12f
maxpe3447 <mpechersky16@gmail.com>
2024-02-03 19:42:03 +0200

    add les05

---
+++ Les05/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner cs = new Scanner(System.in);
+
+        //for (...;...;...)
+        //for(int i = 3;i < 5;i++){
+        //    System.out.println(i+") Hello");
+        // }
+    }
+}
