883313c376a8129d6ae75c97ed57ccbab70528c4
mentorAC <121520054+mentorAC@users.noreply.github.com>
2024-02-18 14:54:47 +0200

    add array

--- Les07/src/Main.java
+++ Les07/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //int[] arr = {1,4,2,5,6,4,1,6};
+        //System.out.println(arr.length);
+        //System.out.println(arr[3]);
+
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[4]);
+//        System.out.println(arr[3]);
+        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt();
+//
+//        int[] arr = new int[size];
+//
+//        for (int i = 0; i < arr.length; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        System.out.println();
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+
+        
     }
 }
