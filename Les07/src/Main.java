392420ec7ec4ec3566dae7fec28633af30fc6355
Valeriia04 <savcinvaleria@gmail.com>
2024-03-08 21:43:09 +0200

    hw7

--- Les07/src/Main.java
+++ Les07/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,7 @@ public class Main {
 //        System.out.println(arr[4]);
 //        System.out.println(arr[3]);
         Scanner sc = new Scanner(System.in);
+
 //        int size = sc.nextInt();
 //
 //        int[] arr = new int[size];
@@ -24,7 +26,56 @@ public class Main {
 //        for(int i = 0; i < arr.length; i++){
 //            System.out.print(arr[i]+" ");
 //        }
+//        int size = sc.nextInt();
+//        int[] arr = new int[size];
+//        for (int i = 0; i < arr.length; i ++) {
+//            arr[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < arr.length; i += 2) {
+//            System.out.println(arr[i]);
+//
+//        }
+//        for(int i = arr.length-1; i>=0; i--)
+//        {
+//            System.out.print(arr[i]+ " ");
+//        }
 
-        
+//        int size = sc.nextInt();
+//        int[] arr = new int[size];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < arr.length; i += 2) {
+//            if (arr[i] % 2 != 0) {
+//                System.out.print(arr[i] + " ");
+//            }
+//        }
+//        System.out.println();
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2 == 0) {
+//                System.out.print(arr[i] + " ");
+//            }
+//
+//        }
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        Random a = new Random();
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = a.nextInt(-100, 100);
+            System.out.print( arr[i]  + " ");
+
+        }
+        System.out.println( );
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                counter++;
+            }
+
+
+        }
+        System.out.println( counter + " ");
     }
 }
