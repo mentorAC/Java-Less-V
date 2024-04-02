719dba984edd4f6eac22a580a8978116f303073d
Valeriia04 <savcinvaleria@gmail.com>
2024-04-02 11:13:14 +0300

    les 11

--- les 11/src/Main.java
+++ les 11/src/Main.java
@@ -1,58 +1,71 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        char[][] matrix = new char[size][size];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j]= ' ';
-                if (i == 0 || i == size - 1) {
-                    matrix[i][j] = '*';
-                }
-                if (i == j) {
-                    matrix[i][j] = '*';
-                }
-                if (i + j == size - 1) {
-                    matrix[i][j] = '*';
-
-                }
-
-            }
-
-
-
-
-        /*int size = sc.nextInt();
-        int[][] matrix = new int[size][size];
-        int a = 0;
-        boolean b = true;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-               b = !b;
-                if (b == false) {
-                    a++;
-                     matrix[i][j] = a;
-                }
-
-            }
-
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + " ");
-
-            }
-            System.out.println();
-
-        }*/
-        }
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt();
+//        char[][] matrix = new char[size][size];
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix.length; j++) {
+//                matrix[i][j]= ' ';
+//                if (i == 0 || i == size - 1) {
+//                    matrix[i][j] = '*';
+//                }
+//                if (i == j) {
+//                    matrix[i][j] = '*';
+//                }
+//                if (i + j == size - 1) {
+//                    matrix[i][j] = '*';
+//
+//                }
+//
+//            }
+//
+//
+//
+//
+//        /*int size = sc.nextInt();
+//        int[][] matrix = new int[size][size];
+//        int a = 0;
+//        boolean b = true;
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix.length; j++) {
+//               b = !b;
+//                if (b == false) {
+//                    a++;
+//                     matrix[i][j] = a;
+//                }
+//
+//            }
+//
+//        }
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix.length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//
+//            }
+//            System.out.println();
+//
+//        }*/
+//        }
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix.length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        maxFun(5,8);
     }
+
+    static void maxFun( int a, int b){
+        if( a> b){
+            System.out.println( " Max = " + a);
+        } else if( b>a){
+            System.out.println( " Max = " + b);
+
+        }
+
+    }
+
+
 }
