eec62535f95379a3e1b3fd32aeb7417304f36792
Valeriia04 <savcinvaleria@gmail.com>
2024-05-19 16:51:02 +0300

    les 12 OOP

--- Les12_OOP/src/Main.java
+++ Les12_OOP/src/Main.java
@@ -10,6 +10,39 @@ public class Main {
         human2.PrintName();
         human2.PrintName();
 
+Lampa lampa1 = new Lampa();
+lampa1.ShowStatus();
+lampa1.TurnOn();
+        lampa1.ShowStatus();
+        lampa1.TurnOff();
+        lampa1.ShowStatus();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.TurnOn();
+        lampa1.ShowStatus();
+        TV tv1 = new TV();
+        tv1.ShowChannel();
+        tv1.Down();
+        tv1.ShowChannel();
+        tv1.Up();
+        tv1.ShowChannel();
+        tv1.CurrentChannel(121);
+        tv1.ShowChannel();
+
+
+
+
+
+
+
+
 
     }
 }
@@ -27,5 +60,60 @@ class Human{
     public void PrintName(){
         System.out.println(name);
         name = "Bob";
+    }
+}
+
+class Lampa{
+   private boolean status = false;
+   private int count= 0;
+    public void TurnOn(){
+        status= true;
+        count++;
+    }
+    public void TurnOff(){
+        status = false;
+    }
+    public void ShowStatus(){
+        if(count>=10){
+            System.out.println(" Lampa broke");
+            return;
+        }
+        if(status== true){
+            System.out.println(" Working");
+
+        }else{
+            System.out.println("Not Working");
+
+        }
+
+    }
+}
+class TV{
+    public int channel = 1;
+    public void Up(){
+        channel++;
+        if(channel>=101){
+            channel=1;
+        }
+    }
+    public void Down(){
+        channel--;
+        if (channel <= 0) {
+            channel=100;
+        }
+    }
+    public void CurrentChannel(int channel){
+       this.channel=channel;
+       if(channel<=0){
+           this.channel=1;
+       }
+       else if (channel>=100){
+           this.channel=100;
+       }else{
+           this.channel=channel;
+       }
+    }
+    public void ShowChannel(){
+        System.out.println(channel);
     }
 }
